18e9efaacfe54b723cf907302f3999748aadd099
Tomcorian <tomek.chmura2004@gmail.com>
2024-05-04 19:27:25 +0200

    FIxed previous issues
    Dostałem opierdol xD

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class testScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Uruchomiono");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
