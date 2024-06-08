539a3e53f9495811c415a95816f253c461b47787
Jakub <jakmil87@gmail.com>
2024-06-08 22:20:09 +0200

    Fixed statistics info
    Added a couple of new information that is saved after finishing the tournament

--- Assets/Scripts/Tournament.cs
+++ Assets/Scripts/Tournament.cs
@@ -59,135 +59,7 @@ public class Tournament : MonoBehaviour
         }
     }
 
-    void SaveWinnerToFile()
-    {
-        //save winner to .txt file
-        var fileLocation = "WinnersFile.txt";
-        using StreamWriter writer = new(fileLocation, append: true);
-        if (File.Exists(fileLocation))
-        {
-            if (new FileInfo(fileLocation).Length == 0)
-            {
-                writer.WriteLine(
-                    "Id postaci;Imie postaci;Klasa postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa");
-            }
-
-            writer.WriteLine(idList[0] + ";" + tournamentList[0].name + ";" + tournamentList[0].GetType() + ";" +
-                             tournamentList[0].receivedDamage + ";" + tournamentList[0].dealtDamage + ";" +
-                             tournamentList[0].usedNormalAttacks + ";" + tournamentList[0].usedStrongAttacks + ";" +
-                             tournamentList[0].usedScreamAttacks + ";" + tournamentList[0].usedSpecialAttacks + ";" +
-                             tournamentList[0].missedAttacks + ";" + ButtonsScript.mapName);
-        }
-
-        SaveToFile(0);
-    }
-
-    void SaveWarriorsToFile(int i)
-    {
-        //save warriors to .txt file
-        var fileLocation = "WarriorsFile.txt";
-        using StreamWriter writer = new(fileLocation, append: true);
-        if (File.Exists(fileLocation))
-        {
-            if (new FileInfo(fileLocation).Length == 0)
-            {
-                writer.WriteLine(
-                    "Id postaci;Imie postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa;Runda");
-            }
-
-            writer.WriteLine(idList[i] + ";" + tournamentList[i].name + ";" + tournamentList[i].receivedDamage + ";" +
-                             tournamentList[i].dealtDamage + ";" + tournamentList[0].usedNormalAttacks + ";" +
-                             tournamentList[0].usedStrongAttacks + ";" + tournamentList[0].usedScreamAttacks + ";" +
-                             tournamentList[0].usedSpecialAttacks + ";" + tournamentList[0].missedAttacks + ";" +
-                             ButtonsScript.mapName + ";" + (round + 1));
-        }
-    }
-
-    void SaveMagesToFile(int i)
-    {
-        //save mages to .txt file
-        var fileLocation = "MagesFile.txt";
-        using StreamWriter writer = new(fileLocation, append: true);
-        if (File.Exists(fileLocation))
-        {
-            if (new FileInfo(fileLocation).Length == 0)
-            {
-                writer.WriteLine(
-                    "Id postaci;Imie postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa;Runda");
-            }
-
-            writer.WriteLine(idList[i] + ";" + tournamentList[i].name + ";" + tournamentList[i].receivedDamage + ";" +
-                             tournamentList[i].dealtDamage + ";" + tournamentList[0].usedNormalAttacks + ";" +
-                             tournamentList[0].usedStrongAttacks + ";" + tournamentList[0].usedScreamAttacks + ";" +
-                             tournamentList[0].usedSpecialAttacks + ";" + tournamentList[0].missedAttacks + ";" +
-                             ButtonsScript.mapName + ";" + (round + 1));
-        }
-    }
-
-    void SaveRoguesToFile(int i)
-    {
-        //save rouges to .txt file
-        var fileLocation = "RougesFile.txt";
-        using StreamWriter writer = new(fileLocation, append: true);
-        if (File.Exists(fileLocation))
-        {
-            if (new FileInfo(fileLocation).Length == 0)
-            {
-                writer.WriteLine(
-                    "Id postaci;Imie postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa;Runda");
-            }
-
-            writer.WriteLine(idList[i] + ";" + tournamentList[i].name + ";" + tournamentList[i].receivedDamage + ";" +
-                             tournamentList[i].dealtDamage + ";" + tournamentList[0].usedNormalAttacks + ";" +
-                             tournamentList[0].usedStrongAttacks + ";" + tournamentList[0].usedScreamAttacks + ";" +
-                             tournamentList[0].usedSpecialAttacks + ";" + tournamentList[0].missedAttacks + ";" +
-                             ButtonsScript.mapName + ";" + (round + 1));
-        }
-    }
-
-    void SaveBardToFile(int i)
-    {
-        //save bard to .txt file
-        var fileLocation = "BardsFile.txt";
-
-        using StreamWriter writer = new(fileLocation, append: true);
-        if (File.Exists(fileLocation))
-        {
-            if (new FileInfo(fileLocation).Length == 0)
-            {
-                writer.WriteLine(
-                    "Id postaci;Imie postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa;Runda");
-            }
-
-            writer.WriteLine(idList[i] + ";" + tournamentList[i].name + ";" + tournamentList[i].receivedDamage + ";" +
-                             tournamentList[i].dealtDamage + ";" + tournamentList[0].usedNormalAttacks + ";" +
-                             tournamentList[0].usedStrongAttacks + ";" + tournamentList[0].usedScreamAttacks + ";" +
-                             tournamentList[0].usedSpecialAttacks + ";" + tournamentList[0].missedAttacks +
-                             ButtonsScript.mapName + ";" + (round + 1));
-        }
-    }
-
-    void SaveToFile(int i)
-    {
-        if (tournamentList[i] is Warrior)
-        {
-            SaveWarriorsToFile(i);
-        }
-        else if (tournamentList[i] is Mage)
-        {
-            SaveMagesToFile(i);
-        }
-        else if (tournamentList[i] is Rouge)
-        {
-            SaveRoguesToFile(i);
-        }
-        else
-        {
-            SaveBardToFile(i);
-        }
-    }
-
-
+    Participant secondPlace;
     void StartRound()
     {
         // clear idToDelete table each round to make sure the ids dont overlap
@@ -200,14 +72,23 @@ public class Tournament : MonoBehaviour
             if (tournamentList[i].id != deletedID)
             {
                 ColdBeerToRefillYourWillToFight(tournamentList[i]);
-                SaveToFile(i + 1);
+                SaveToFile(i + 1, tournamentList[i]);
             }
             else
             {
                 ColdBeerToRefillYourWillToFight(tournamentList[i + 1]);
-                SaveToFile(i);
+                SaveToFile(i, tournamentList[i + 1]);
             }
-
+            if(round==3)
+            {
+                for(int j=0;j< tournamentList.Count;j++)
+                {
+                    if (tournamentList[j].id==deletedID)
+                    {
+                        secondPlace = tournamentList[j];
+                    }    
+                }
+            }
             idToDelte.Add(deletedID);
         }
 
@@ -237,7 +118,7 @@ public class Tournament : MonoBehaviour
         {
             //Debug.Log(tournamentList[0].name + " Wins!");
             winnerImage.sprite = tournamentList[0].sprite;
-            SaveWinnerToFile();
+            SaveWinnerToFile(secondPlace);
         }
     }
 
@@ -313,6 +194,136 @@ public class Tournament : MonoBehaviour
             particpantList.Add(new Bard(bardScriptable[i]));
         }
     }
+
+
+    void SaveWinnerToFile(Participant secondPlace)
+    {
+        //save winner to .txt file
+        var fileLocation = "WinnersFile.txt";
+        using StreamWriter writer = new(fileLocation, append: true);
+        if (File.Exists(fileLocation))
+        {
+            if (new FileInfo(fileLocation).Length == 0)
+            {
+                writer.WriteLine(
+                    "Id postaci;Imie postaci;Klasa postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa;Z kim wygra³?;Z jak¹ klasa?") ;
+            }
+
+            writer.WriteLine(idList[0] + ";" + tournamentList[0].name + ";" + tournamentList[0].GetType() + ";" +
+                             tournamentList[0].receivedDamage + ";" + tournamentList[0].dealtDamage + ";" +
+                             tournamentList[0].usedNormalAttacks + ";" + tournamentList[0].usedStrongAttacks + ";" +
+                             tournamentList[0].usedScreamAttacks + ";" + tournamentList[0].usedSpecialAttacks + ";" +
+                             tournamentList[0].missedAttacks + ";" + ButtonsScript.mapName + ";" + secondPlace.name + ";" + secondPlace.GetType());
+        }
+        secondPlace.name = "Godny kuboty";
+        SaveToFile(0, secondPlace);
+    }
+
+    void SaveWarriorsToFile(int i, Participant whomLosedTo)
+    {
+        //save warriors to .txt file
+        var fileLocation = "WarriorsFile.txt";
+        using StreamWriter writer = new(fileLocation, append: true);
+        if (File.Exists(fileLocation))
+        {
+            if (new FileInfo(fileLocation).Length == 0)
+            {
+                writer.WriteLine(
+                    "Id postaci;Imie postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa;Runda;Z kim przegral;Z jak¹ klasa?");
+            }
+
+            writer.WriteLine(idList[i] + ";" + tournamentList[i].name + ";" + tournamentList[i].receivedDamage + ";" +
+                             tournamentList[i].dealtDamage + ";" + tournamentList[0].usedNormalAttacks + ";" +
+                             tournamentList[0].usedStrongAttacks + ";" + tournamentList[0].usedScreamAttacks + ";" +
+                             tournamentList[0].usedSpecialAttacks + ";" + tournamentList[0].missedAttacks + ";" +
+                             ButtonsScript.mapName + ";" + (round + 1) + ";" + whomLosedTo.name + ";" + whomLosedTo.GetType().Name);
+        }
+    }
+
+    void SaveMagesToFile(int i, Participant whomLosedTo)
+    {
+        //save mages to .txt file
+        var fileLocation = "MagesFile.txt";
+        using StreamWriter writer = new(fileLocation, append: true);
+        if (File.Exists(fileLocation))
+        {
+            if (new FileInfo(fileLocation).Length == 0)
+            {
+                writer.WriteLine(
+                    "Id postaci;Imie postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa;Runda;Z kim przegral;Z jak¹ klasa?");
+            }
+
+            writer.WriteLine(idList[i] + ";" + tournamentList[i].name + ";" + tournamentList[i].receivedDamage + ";" +
+                             tournamentList[i].dealtDamage + ";" + tournamentList[0].usedNormalAttacks + ";" +
+                             tournamentList[0].usedStrongAttacks + ";" + tournamentList[0].usedScreamAttacks + ";" +
+                             tournamentList[0].usedSpecialAttacks + ";" + tournamentList[0].missedAttacks + ";" +
+                             ButtonsScript.mapName + ";" + (round + 1) + ";" + whomLosedTo.name + ";" + whomLosedTo.GetType().Name);
+        }
+    }
+
+    void SaveRoguesToFile(int i, Participant whomLosedTo)
+    {
+        //save rouges to .txt file
+        var fileLocation = "RougesFile.txt";
+        using StreamWriter writer = new(fileLocation, append: true);
+        if (File.Exists(fileLocation))
+        {
+            if (new FileInfo(fileLocation).Length == 0)
+            {
+                writer.WriteLine(
+                    "Id postaci;Imie postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa;Runda;Z kim przegral;Z jak¹ klasa?");
+            }
+
+            writer.WriteLine(idList[i] + ";" + tournamentList[i].name + ";" + tournamentList[i].receivedDamage + ";" +
+                             tournamentList[i].dealtDamage + ";" + tournamentList[0].usedNormalAttacks + ";" +
+                             tournamentList[0].usedStrongAttacks + ";" + tournamentList[0].usedScreamAttacks + ";" +
+                             tournamentList[0].usedSpecialAttacks + ";" + tournamentList[0].missedAttacks + ";" +
+                             ButtonsScript.mapName + ";" + (round + 1) + ";" + whomLosedTo.name + ";" + whomLosedTo.GetType().Name);
+        }
+    }
+
+    void SaveBardToFile(int i, Participant whomLosedTo)
+    {
+        //save bard to .txt file
+        var fileLocation = "BardsFile.txt";
+
+        using StreamWriter writer = new(fileLocation, append: true);
+        if (File.Exists(fileLocation))
+        {
+            if (new FileInfo(fileLocation).Length == 0)
+            {
+                writer.WriteLine(
+                    "Id postaci;Imie postaci;Otrzymane obrazenia;Zadane obrazenia;U¿yte ataki podstawowe;U¿yte silne ataki;U¿yte ataki krzykiem;U¿yte ataki specjalne;Ile spud³owano;Mapa;Runda;Z kim przegral;Z jak¹ klasa?");
+            }
+
+            writer.WriteLine(idList[i] + ";" + tournamentList[i].name + ";" + tournamentList[i].receivedDamage + ";" +
+                             tournamentList[i].dealtDamage + ";" + tournamentList[0].usedNormalAttacks + ";" +
+                             tournamentList[0].usedStrongAttacks + ";" + tournamentList[0].usedScreamAttacks + ";" +
+                             tournamentList[0].usedSpecialAttacks + ";" + tournamentList[0].missedAttacks +
+                             ButtonsScript.mapName + ";" + (round + 1) + ";" + whomLosedTo.name + ";" + whomLosedTo.GetType().Name);
+        }
+    }
+
+    void SaveToFile(int i, Participant participant)
+    {
+        if (tournamentList[i] is Warrior)
+        {
+            SaveWarriorsToFile(i, participant);
+        }
+        else if (tournamentList[i] is Mage)
+        {
+            SaveMagesToFile(i, participant);
+        }
+        else if (tournamentList[i] is Rouge)
+        {
+            SaveRoguesToFile(i, participant);
+        }
+        else
+        {
+            SaveBardToFile(i, participant);
+        }
+    }
+
 
     //this functions is very complicated as it picks the best photo of the most perfect human beeing to oversee the whole tournament
     void MaklowiczMood()
